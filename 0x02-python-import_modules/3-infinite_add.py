4519e47e909609f812470ce14986b68430bc3ee6
tgairwoman <tgjehovah@gmail.com>
2023-10-06 06:04:53 +0300

    import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """ Add all arguments."""
     import sys
+    argc = len(sys.argv)
+    argv = sys.argv
+    res = 0
 
-    result = 0
-
-    for i in range(len(sys.argv) - 1):
-        result += (int(sys.argv[i + 1]))
-    print("{:d}".format(result))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    for i in range(1, argc):
+        res = res + int(argv[i])
+        print(res)
