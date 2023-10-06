dbc330509f83009f74063797162a3df264d4cd5c
tgairwoman <tgjehovah@gmail.com>
2023-10-06 06:08:00 +0300

    import

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,8 @@
-#!/bin/usr/bin/ptyhon3
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-    if (a < b):
+    if a < b:
         c = add(a, b)
-        for z in range(4, 6):
-            c = add(c, z)
-        return(c)
-    return(sub(a, b))
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    return sub(a, b)
