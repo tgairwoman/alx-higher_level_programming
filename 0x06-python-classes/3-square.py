ca6f1619aaca27e8b83c9211436c1c0e218598f4
tgairwoman <tgjehovah@gmail.com>
2023-10-24 20:12:15 +0300

    docstringshit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,7 +11,7 @@ class Square:
         Args:
             size (int, optional): The size of the square. Defaults to 0.
         """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
