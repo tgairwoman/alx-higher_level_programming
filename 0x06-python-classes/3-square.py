3b1dec87aa93228f9c200632d07037aec58859a6
tgairwoman <tgjehovah@gmail.com>
2023-10-24 20:09:16 +0300

    docstringshit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,12 +2,7 @@
 
 
 class Square:
-    """
-    This class represents a square.
-
-    Attributes:
-        __size (int): The size of the square.
-    """
+    """ This class represents a square """
 
     def __init__(self, size=0):
         """
@@ -21,12 +16,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """
-        Calculates and returns the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
