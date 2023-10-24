8e5b0d661fbb5b351ebbcf2bad279a73ea301d31
tgairwoman <tgjehovah@gmail.com>
2023-10-24 21:16:42 +0300

    docstringshit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,3 +13,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """ Calculating the area of the square """
+        return self.__size ** 2
