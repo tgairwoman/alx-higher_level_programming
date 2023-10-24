02b184778de6dd5924a37a058e432b186dbec9ab
tgairwoman <tgjehovah@gmail.com>
2023-10-24 17:15:04 +0300

    docstringshit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """
-    This class represents a square.
-
-    Attributes:
-        __size (int): The size of the square.
+    This class represents a square
     """
 
     def __init__(self, size=0):
@@ -13,10 +10,6 @@ class Square:
 
         Args:
             size (int, optional): The size of the square. Defaults to 0.
-        
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
