1bc06dc9b6a6419bd1e9859ad431e0f077577a64
tgairwoman <tgjehovah@gmail.com>
2023-10-24 21:09:00 +0300

    docstringshit

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""
-create a class Square
-"""
-
-
+"""Magic class from a given ByteCode."""
 import math
 
+
 class MagicClass:
+    """Initialization of the MagicClass."""
     def __init__(self, radius=0):
-        self.__radius = 0
+        """Initialization of the data."""
+        self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
-        self.__radius = radius
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
 
     def area(self):
-        return self.__radius ** 2 * math.pi
+        """Calculation of the area."""
+        return self._MagicClass__radius ** 2 * math.pi
 
     def circumference(self):
-        return 2 * math.pi * self.__radius
+        """Calculation of the circumference."""
+        return 2 * math.pi * self._MagicClass__radius
