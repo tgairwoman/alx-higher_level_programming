23909d08ad572b9d72d309b24886dd1ea4e62fcc
tgairwoman <tgjehovah@gmail.com>
2023-10-24 17:02:33 +0300

    docstringshit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
