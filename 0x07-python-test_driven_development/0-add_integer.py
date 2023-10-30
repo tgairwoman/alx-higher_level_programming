10c77eb17115c95379f5de1070fd99c32e91da15
tgairwoman <tgjehovah@gmail.com>
2023-10-30 23:22:49 +0300

    realworld

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,11 @@
+#!/usr/bin/python3
+"""A function that adds two integers."""
+
+
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-
-    Args:
-        a (int or float): The first number.
-        b (int or float): The second number. Defaults to 98.
-
-    Returns:
-        int: The sum of a and b.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
-
-    """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
-
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    """A function that return int(a) + int(b)."""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,1 @@
-#!/usr/bin/python3
-def raise_exception():
-    raise TypeError
+
