d3d879266fbc73c14db6a112933246c9078a710f
tgairwoman <tgjehovah@gmail.com>
2023-12-12 21:42:04 +0300

    sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,14 +1,6 @@
-#!/usr/bin/python3
-"""Defines a matrix multiplication function using NumPy."""
-import numpy as np
-
-
-def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
-
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    """
-
-    return (np.matmul(m_a, m_b))
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
